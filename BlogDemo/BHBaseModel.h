f7b768a20aa00e21396aa574dc3b23b66cd6723f
HanBin <han5yi2xuan1@163.com>
2016-05-14 22:11:29 +0800

    no message

---
+++ BlogDemo/BHBaseModel.h
@@ -0,0 +1,11 @@
+//
+//  BHBaseModel.h
+//  BlogDemo
+//
+//  Created by HanBin on 16/5/14.
+//  Copyright © 2016年 BinHan. All rights reserved.
+//
+
+@interface BHBaseModel : NSObject
+
+@end
---
+++ BlogDemo/BHBaseModel.m
@@ -0,0 +1,34 @@
+//
+//  BHBaseModel.m
+//  BlogDemo
+//
+//  Created by HanBin on 16/5/14.
+//  Copyright © 2016年 BinHan. All rights reserved.
+//
+
+@implementation BHBaseModel
+
+- (NSString *)description
+{
+    id modelClass = [self class];
+    unsigned int outCount, i;
+    objc_property_t *properties = class_copyPropertyList(modelClass, &outCount);
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:outCount];
+    //遍历出所有的属性key/value
+    for (i = 0; i < outCount; i++)
+    {
+        objc_property_t property = properties[i];
+        NSString *propName = [NSString stringWithUTF8String:property_getName(property)];
+        id value = [[self valueForKey:propName] description];
+        [dict setObject:value forKey:propName];
+    }
+    return [NSString stringWithFormat:@"<%@: %p> %@", NSStringFromClass([self class]), self, dict];
+}
+
+-(NSString *)debugDescription
+{
+    return [self description];
+}
+
+
+@end
