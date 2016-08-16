09c2db1daf0f3ae7e33fd785595416f5ec5fce5b
HanBin <han5yi2xuan1@163.com>
2016-08-17 00:05:45 +0800

    no message

--- BlogDemo/BHViewController10.m
+++ BlogDemo/BHViewController10.m
@@ -13,6 +13,18 @@
 
 -(void)viewDidLoad
 {
+    
+    [self.userNameTF.rac_textSignal subscribeNext:^(id x) {
+        BHLog(@"x = %@", x);
+    }];
+    
+    [[self.userNameTF.rac_textSignal filter:^BOOL(id value) {
+        NSString *userName = value;
+        return userName.length > 3;
+    }] subscribeNext:^(id x) {
+        BHLog(@"用户名大于3了");
+    }];
+    
     [[self.loginBT rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
         BHLog(@"点击了登录");
     }];
