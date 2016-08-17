653e06f51e9ecfe6076fde579121e25255511355
HanBin <han5yi2xuan1@163.com>
2016-08-17 11:53:14 +0800

    no message

--- BlogDemo/BHViewController10.m
+++ BlogDemo/BHViewController10.m
@@ -8,14 +8,20 @@
 
 #import "BHViewController10.h"
 
+@interface BHViewController10()
+
+@property (nonatomic, copy) NSString *userName;
+
+@end
+
 @implementation BHViewController10
 
 
 -(void)viewDidLoad
 {
     
-    [self.userNameTF.rac_textSignal subscribeNext:^(id x) {
-        BHLog(@"x = %@", x);
+    [self.userNameTF.rac_textSignal subscribeNext:^(NSString *text) {
+        self.userName = text;
     }];
     
     [[self.userNameTF.rac_textSignal filter:^BOOL(id value) {
@@ -28,6 +34,24 @@
     [[self.loginBT rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
         BHLog(@"点击了登录");
     }];
+    
+    RAC(self.loginBT, backgroundColor) = [self.userNameTF.rac_textSignal map:^id(NSString *text) {
+        return text.length > 3 ? [UIColor redColor] : [UIColor blueColor];
+    }];
+    
+    //KVO
+    [RACObserve(self, userName) subscribeNext:^(id x) {
+        BHLog(@"userName = %@", x);
+    }];
+    
+    //同时满足长度大于5按钮才可以点击
+    RAC(self.loginBT, enabled) = [RACSignal combineLatest:@[self.userNameTF.rac_textSignal, self.passWordTF.rac_textSignal] reduce:^(NSString *usernameValid, NSString *passwordValid) {
+        return @(usernameValid.length > 5 && passwordValid.length > 5);
+    }];
+    
+    @weakify(self);
+    
+    
 }
 
 @end
